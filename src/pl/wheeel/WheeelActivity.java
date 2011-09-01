25076c2724b594853464b9f599120a9e96725a7a
kbl <m.pietraszek@gmail.com>
2011-09-01 16:01:57 +0200

    some tests with MapView, displaying google map as main app view

--- src/pl/wheeel/WheeelActivity.java
+++ src/pl/wheeel/WheeelActivity.java
@@ -1,13 +1,22 @@
 package pl.wheeel;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class WheeelActivity extends Activity {
-    /** Called when the activity is first created. */
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class WheeelActivity extends MapActivity {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.map);
+
+        MapView mapView = (MapView) findViewById(R.id.mapView);
+        mapView.setBuiltInZoomControls(true);
     }
+
+	@Override
+	protected boolean isRouteDisplayed() { return false; }
+
 }
