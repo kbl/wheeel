006e74718cefeed16a86a9bd11c05ee5ba1abe23
kbl <m.pietraszek@gmail.com>
2011-09-02 15:34:33 +0200

    counting price

--- src/pl/wheeel/WheeelMainActivity.java
+++ src/pl/wheeel/WheeelMainActivity.java
@@ -2,52 +2,71 @@ package pl.wheeel;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.Chronometer.OnChronometerTickListener;
+import android.widget.TextView;
 
 public class WheeelMainActivity extends Activity implements OnChronometerTickListener {
 
     private Chronometer mCounter;
-	private Button mResetButton;
 	private Button mStartButton;
+	private TextView mPrice;
 
 	private static final int FIRST_HOURLY_PRICE = 2;
 	private static final int HOURLY_PRICE = 4;
+
+	private static final long TWENTY_MINUTES = 10 * 1000;
+	private static final long ONE_HOUR = 3 * TWENTY_MINUTES;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        mCounter = (Chronometer) findViewById(R.id.counterTime);
-        mCounter.setOnChronometerTickListener(this);
+        initCounter();
         initButtons();
+        mPrice = (TextView) findViewById(R.id.counterPrice);
     }
+
+	private void initCounter() {
+		mCounter = (Chronometer) findViewById(R.id.counterTime);
+        mCounter.setOnChronometerTickListener(this);
+	}
 
 	private void initButtons() {
 		mStartButton = (Button) findViewById(R.id.counterStartButton);
         mStartButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
-				mCounter.setBase(System.currentTimeMillis());
+				mCounter.setBase(SystemClock.elapsedRealtime());
 		        mCounter.start();
 		        mStartButton.setEnabled(false);
 			}
 		});
 
-        mResetButton = (Button) findViewById(R.id.counterResetButton);
+        Button mResetButton = (Button) findViewById(R.id.counterResetButton);
         mResetButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 		        mCounter.stop();
 		        mStartButton.setEnabled(true);
+		        mPrice.setText(R.string.counterPriceInitMsg);
 			}
 		});
 	}
 
 	public void onChronometerTick(Chronometer chronometer) {
-		CharSequence x = chronometer.getText();
-		CharSequence y = mCounter.getText();
+		long time = SystemClock.elapsedRealtime() - chronometer.getBase();
+		if(time > TWENTY_MINUTES) {
+			if(time > ONE_HOUR) {
+				time = (time % ONE_HOUR) - 1;
+				mPrice.setText((FIRST_HOURLY_PRICE + time * HOURLY_PRICE) + " zl");
+			}
+			else {
+				mPrice.setText(FIRST_HOURLY_PRICE + " zl");
+			}
+		}
 	}
 
 }
