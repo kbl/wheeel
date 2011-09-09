0b7be7c96574cf06f8655ac4f41e18cba9abc45b
kbl <m.pietraszek@gmail.com>
2011-09-09 08:57:07 +0200

    null means read all columns - exactly what is needed in this case

--- src/pl/wheeel/location/DockingStationDao.java
+++ src/pl/wheeel/location/DockingStationDao.java
@@ -17,17 +17,9 @@ public class DockingStationDao {
 
 	private static final String TABLE = "docking_station";
 
-	private static final String ID_COLUMN = "_id";
 	public static final int ID_INDEX = 0;
-
-	private static final String LAT_COLUMN = "lat";
 	public static final int LAT_INDEX = 1;
-
-	private static final String LON_COLUMN = "lon";
 	public static final int LON_INDEX = 2;
-
-	private static final String[] COLUMNS =
-			{ID_COLUMN, LAT_COLUMN, LON_COLUMN};
 
 	private WheeelDbHelper dbHelper;
 	private SQLiteDatabase db;
@@ -48,7 +40,7 @@ public class DockingStationDao {
 	}
 
 	public Cursor getAllDockingStations() {
-		return db.query(TABLE, COLUMNS, null, null, null, null, null);
+		return db.query(TABLE, null, null, null, null, null, null);
 	}
 
 }
