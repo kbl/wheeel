efb50695e631b95360a9df097d15f4a01b1ca109
kbl <m.pietraszek@gmail.com>
2011-09-09 09:36:39 +0200

    autoincrement isnt necessary as all records id are maneged by my own - its crucial to have exact ids as they are connected with idices in resources arrays

--- src/pl/wheeel/db/WheeelDbHelper.java
+++ src/pl/wheeel/db/WheeelDbHelper.java
@@ -16,7 +16,7 @@ public class WheeelDbHelper extends SQLiteOpenHelper {
 	private static final int VERSION = 1;
 	private static final String DB_NAME = "wheeel.db";
 	private static final String CREATE_DB_STMT = "CREATE TABLE docking_station (" +
-			"_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE," +
+			"_id INTEGER PRIMARY KEY NOT NULL UNIQUE," +
 			"lat INTEGER NOT NULL," +
 			"lon INTEGER NOT NULL);";
 
